6a0f2c0f895915f6329a923e9cf0c5e470a1f186
Michael Watson <m.watson.tn@gmail.com>
2018-08-09 14:05:05 -0500

    fixed disclaimerFragment tests after kotlin conversion

--- app/src/main/java/com/rosebay/odds/ui/disclaimer/DisclaimerPresenterImpl.kt
+++ app/src/main/java/com/rosebay/odds/ui/disclaimer/DisclaimerPresenterImpl.kt
@@ -12,7 +12,7 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
-class DisclaimerPresenterImpl : AbstractPresenter<DisclaimerView>(), DisclaimerPresenter {
+open class DisclaimerPresenterImpl : AbstractPresenter<DisclaimerView>(), DisclaimerPresenter {
 
     @Inject
     lateinit var firebaseClient: FirebaseClient
